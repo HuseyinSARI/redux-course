e6773b58ecfc35eee752ff96e7395e1f8b5769ca
HuseyinSARI <huseyin-sari61@hotmail.com>
2022-10-05 21:44:45 +0300

    install react, arrange appropriate components, and add include necessary css

---
+++ todo-app/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <form>
+            <input className='new-todo' placeholder='what needs to be done?' autoFocus />
+        </form>
+    )
+}
+
+export default Form
